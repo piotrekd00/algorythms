ae7328de7518f30b89c56f24150da7ddcfcac66e
piotrekd00 <p.dudkiewicz.156@studms.ug.edu.pl>
2023-01-06 16:10:32 +0100

    bst

--- bst/main.c
+++ bst/main.c
@@ -11,10 +11,12 @@ typedef struct Node{
 
 typedef struct BST{
   struct Node *root;
+  int count;
 }BST;
 
 void init(BST **tree){
   *tree = malloc(sizeof(BST));
+  (*tree)->count = 0;
 }
 
 Node *new_node(int data){
@@ -25,17 +27,19 @@ Node *new_node(int data){
   return n_node;
 }
 
-void insert_node(Node **root, int data) {
+void insert_node(BST *tree, Node **root, int data) {
   if (*root == NULL) {
       *root = new_node(data);
+      tree->count++;
       return;
   }
   if (data <= (*root)->value) {
-      insert_node(&(*root)->left, data);
+      insert_node(tree, &(*root)->left, data);
   } else {
-      insert_node(&(*root)->right, data);
+      insert_node(tree, &(*root)->right, data);
   }
 }
+
 
 Node *successor(Node *node) {
   Node *current = node;
@@ -45,49 +49,75 @@ Node *successor(Node *node) {
   return current;
 }
 
-Node *delete_node(Node *root, int data) {
+Node *delete_node(BST *tree, Node *root, int data) {
   if (root == NULL) return root;
   if (data < root->value) {
-    root->left = delete_node(root->left, data);
+    root->left = delete_node(tree, root->left, data);
   } else if (data > root->value) {
-    root->right = delete_node(root->right, data);
+    root->right = delete_node(tree, root->right, data);
   } else {
     if (root->left == NULL) {
       Node *temp = root->right;
       free(root);
+      tree->count--;
       return temp;
     } else if (root->right == NULL) {
       Node *temp = root->left;
       free(root);
+      tree->count--;
       return temp;
     }
     Node *n_successor = successor(root->right);
     root->value = n_successor->value;
-    root->right = delete_node(root->right, n_successor->value);
+    root->right = delete_node(tree, root->right, n_successor->value);
   }
   return root;
 }
 
-void print_inorder(Node *root) {
+void print_inorder(BST *tree, Node *root) {
+  static int count = 0;
   if (root == NULL) return;
-  print_inorder(root->left);
-  printf("%d ", root->value);
-  print_inorder(root->right);
+  print_inorder(tree, root->left);
+  count++;
+  printf("%d", root->value);
+  if (count < tree->count) {
+    printf(" ");
+  } else {
+    printf("\n");
+  }
+  print_inorder(tree, root->right);
 }
 
-void print_postorder(Node *root) {
+
+void print_preorder(BST *tree, Node *root) {
+  static int count = 0;
   if (root == NULL) return;
-  print_postorder(root->left);
-  print_postorder(root->right);
-  printf("%d ", root->value);
+  count++;
+  printf("%d", root->value);
+  if (count < tree->count) {
+    printf(" ");
+  } else {
+    printf("\n");
+  }
+  print_preorder(tree, root->left);
+  print_preorder(tree, root->right);
 }
 
-void print_preorder(Node *root) {
+void print_postorder(BST *tree, Node *root) {
+  static int count = 0;
   if (root == NULL) return;
-  printf("%d ", root->value);
-  print_preorder(root->left);
-  print_preorder(root->right);
+  print_postorder(tree, root->left);
+  print_postorder(tree, root->right);
+  count++;
+  printf("%d", root->value);
+  if (count < tree->count) {
+    printf(" ");
+  } else {
+    printf("\n");
+  }
 }
+
+
 
 void search(Node *root, int data) {
   if (root==NULL) {
@@ -130,22 +160,22 @@ int main (int argc, char *argv[])
       init(&tree);
     }
     else if (strcmp(option, "insert")==0){
-      insert_node(&tree->root, value);
+      insert_node(tree, &tree->root, value);
     }
     else if (strcmp(option, "search")==0){
       search(tree->root, value);
     }
     else if (strcmp(option, "delete")==0){
-      delete_node(tree->root, value);
+      delete_node(tree, tree->root, value);
     }
     else if (strcmp(option, "in_order")==0){
-      print_inorder(tree->root);
+      print_inorder(tree, tree->root);
     }
     else if (strcmp(option, "pre_order")==0){
-      print_preorder(tree->root);
+      print_preorder(tree, tree->root);
     }
     else if (strcmp(option, "post_order")==0){
-      print_postorder(tree->root);
+      print_postorder(tree, tree->root);
     }
     free(lines[j]);
   }

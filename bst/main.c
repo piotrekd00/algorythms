214ca1bf63012bc97367398cfb29b1052250e669
piotrekd00 <p.dudkiewicz.156@studms.ug.edu.pl>
2023-01-06 15:25:52 +0100

    bst

--- bst/main.c
+++ bst/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_LINE_LENGTH 100
 
 typedef struct Node{
-  char* value;
+  int value;
   struct Node *left;
   struct Node *right;
 }Node;
@@ -12,21 +13,141 @@ typedef struct BST{
   struct Node *root;
 }BST;
 
-void init_bst(BST *tree){
-  if (tree == NULL){
-    printf("error init_tree\n");
+void init(BST **tree){
+  *tree = malloc(sizeof(BST));
+}
+
+Node *new_node(int data){
+  Node *n_node = malloc(sizeof(Node));
+  n_node->value = data;
+  n_node->left = NULL;
+  n_node->right = NULL;
+  return n_node;
+}
+
+void insert_node(Node **root, int data) {
+  if (*root == NULL) {
+      *root = new_node(data);
+      return;
   }
-  tree->root = malloc(sizeof(Node));
-  if (tree->root == NULL){
-    printf("error init_tree\n");
+  if (data <= (*root)->value) {
+      insert_node(&(*root)->left, data);
+  } else {
+      insert_node(&(*root)->right, data);
+  }
+}
+
+Node *successor(Node *node) {
+  Node *current = node;
+  while (current->left != NULL) {
+      current = current->left;
+  }
+  return current;
+}
+
+Node *delete_node(Node *root, int data) {
+  if (root == NULL) return root;
+  if (data < root->value) {
+    root->left = delete_node(root->left, data);
+  } else if (data > root->value) {
+    root->right = delete_node(root->right, data);
+  } else {
+    if (root->left == NULL) {
+      Node *temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL) {
+      Node *temp = root->left;
+      free(root);
+      return temp;
+    }
+    Node *n_successor = successor(root->right);
+    root->value = n_successor->value;
+    root->right = delete_node(root->right, n_successor->value);
+  }
+  return root;
+}
+
+void print_inorder(Node *root) {
+  if (root == NULL) return;
+  print_inorder(root->left);
+  printf("%d ", root->value);
+  print_inorder(root->right);
+}
+
+void print_postorder(Node *root) {
+  if (root == NULL) return;
+  print_postorder(root->left);
+  print_postorder(root->right);
+  printf("%d ", root->value);
+}
+
+void print_preorder(Node *root) {
+  if (root == NULL) return;
+  printf("%d ", root->value);
+  print_preorder(root->left);
+  print_preorder(root->right);
+}
+
+void search(Node *root, int data) {
+  if (root==NULL) {
+    printf("False\n");
+    return;
+  }
+  else if (root->value == data) {
+    printf("True\n");
+    return;
+  }
+  if (data <= root->value) {
+    search(root->left, data);
+  } else {
+    search(root->right, data);
   }
 }
 
 int main (int argc, char *argv[])
 {
   BST *tree;
-  tree = malloc(sizeof(BST));
-  init_bst(tree);
-  free(tree);
+  char *lines[MAX_LINE_LENGTH];
+  char line[MAX_LINE_LENGTH];
+  int i = 0;
+
+  while (fgets(line, sizeof(line), stdin) != NULL) {
+    lines[i] = malloc(strlen(line) + 1);
+    strcpy(lines[i], line);
+    i++;
+  }
+
+  for (int j = 0; j < i; j++) {
+    lines[j][strcspn(lines[j], "\n")] = 0;
+    int value;
+    char *option = strtok(lines[j], " ");
+    char *str_value = strtok(NULL, " ");
+    if (str_value != 0){
+      value = atoi(str_value);
+    }
+    if (strcmp(option, "init_tree")==0){
+      init(&tree);
+    }
+    else if (strcmp(option, "insert")==0){
+      insert_node(&tree->root, value);
+    }
+    else if (strcmp(option, "search")==0){
+      search(tree->root, value);
+    }
+    else if (strcmp(option, "delete")==0){
+      delete_node(tree->root, value);
+    }
+    else if (strcmp(option, "in_order")==0){
+      print_inorder(tree->root);
+    }
+    else if (strcmp(option, "pre_order")==0){
+      print_preorder(tree->root);
+    }
+    else if (strcmp(option, "post_order")==0){
+      print_postorder(tree->root);
+    }
+    free(lines[j]);
+  }
   return 0;
 }
